7f7298f4825359556cec6ccfa8f9b57b82319821
a <a@a>
2023-06-12 08:31:47 -0600

    Tutorial01

---
+++ Tutorial1.cpp
@@ -0,0 +1,9 @@
+// Tutorial1.cpp : Define las funciones de la biblioteca estática.
+//
+
+#include "framework.h"
+
+// TODO: Ejemplo de una función de biblioteca
+void fnTutorial1()
+{
+}
---
+++ framework.h
@@ -0,0 +1,7 @@
+// header.h: archivo de inclusión para archivos de inclusión estándar del sistema,
+// o archivos de inclusión específicos de un proyecto
+//
+
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Excluir material rara vez utilizado de encabezados de Windows
